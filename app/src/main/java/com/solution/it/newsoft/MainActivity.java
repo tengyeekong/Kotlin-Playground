a45a183f16376f30f10ac85545389f06fcfbaf25
yao <tengyee.kong92@gmail.com>
2019-07-24 00:26:52 +0800

    commit

--- app/src/main/java/com/solution/it/newsoft/MainActivity.java
+++ app/src/main/java/com/solution/it/newsoft/MainActivity.java
@@ -4,23 +4,27 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getSupportActionBar().hide();
+        DataBindingUtil.setContentView(this, R.layout.activity_main);
 
-        SharedPreferences prefs = getSharedPreferences(BuildConfig.APPLICATION_ID, Context.MODE_PRIVATE);
-        if (!prefs.getString(ViewModel.USERNAME, "").isEmpty()) {
-            Intent intent = new Intent(MainActivity.this, ListingActivity.class);
-            startActivity(intent);
-        } else {
-            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-            startActivity(intent);
-        }
+        new Handler().postDelayed(() -> {
+            SharedPreferences prefs = getSharedPreferences(BuildConfig.APPLICATION_ID, Context.MODE_PRIVATE);
+            if (!prefs.getString(ViewModel.USERNAME, "").isEmpty()) {
+                Intent intent = new Intent(MainActivity.this, ListingActivity.class);
+                startActivity(intent);
+            } else {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        }, 2000);
     }
 }

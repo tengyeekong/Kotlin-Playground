91d04dd8df61a9a2f215fadff1a8c2f62a37e960
yao <tengyee.kong92@gmail.com>
2019-07-24 12:18:37 +0800

    commit

--- app/src/main/java/com/solution/it/newsoft/ListingActivity.java
+++ app/src/main/java/com/solution/it/newsoft/ListingActivity.java
@@ -114,8 +114,6 @@ public class ListingActivity extends AppCompatActivity {
                                 }
                             });
                         }
-                        isLoading = false;
-                        isLastPage = false;
                         dialog.dismiss();
                     });
                 });

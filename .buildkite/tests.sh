f40addf940fff6a328cdc6b9c1db055849bc1823
kongtengyee <tengyee.kong92@gmail.com>
2021-06-23 18:14:49 +0800

    - testing with buildkite pipeline

---
+++ .buildkite/tests.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+PROJECT_ROOT=$(cd -P "$(dirname "$(readlink "${BASH_SOURCE[0]}" || echo "${BASH_SOURCE[0]}")")/.." && pwd)
+echo "Project Root:" "${PROJECT_ROOT}"
+
+cd "$PROJECT_ROOT" || exit
+
+set -exo pipefail
+
+echo "--- Install dependencies"
+yarn
+
+## We use fastlane to run test, because fastlane give us better output
+echo "+++ Tests"
+yarn bundle exec fastlane test
+
+## Zip HTML reports
+#npx run-func zip.js zipFile "$PROJECT_ROOT" "app/build/reports/tests/testProdReleaseUnitTest"
+
+# temporary we just try to build the app, the tests is too heavy
+# yarn fastlane run gradle tasks:app:build

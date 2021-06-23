751c193952ff64b935ac9bef8feeda3e7374739a
kongtengyee <tengyee.kong92@gmail.com>
2021-06-23 18:38:38 +0800

    - testing with buildkite pipeline

--- .buildkite/tests.sh
+++ .buildkite/tests.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-#PROJECT_ROOT=$(cd -P "$(dirname "$(readlink "${BASH_SOURCE[0]}" || echo "${BASH_SOURCE[0]}")")/.." && pwd)
-#echo "Project Root:" "${PROJECT_ROOT}"
-#
-#cd "$PROJECT_ROOT" || exit
+PROJECT_ROOT=$(cd -P "$(dirname "$(readlink "${BASH_SOURCE[0]}" || echo "${BASH_SOURCE[0]}")")/.." && pwd)
+echo "Project Root:" "${PROJECT_ROOT}"
+
+cd "$PROJECT_ROOT" || exit
 
 set -exo pipefail
 
-#echo "--- Install dependencies"
-#yarn
+echo "--- Install dependencies"
+yarn
 
 ## We use fastlane to run test, because fastlane give us better output
 echo "+++ Tests"
-#yarn bundle exec fastlane test
+yarn bundle exec fastlane test
 
 ## Zip HTML reports
 #npx run-func zip.js zipFile "$PROJECT_ROOT" "app/build/reports/tests/testProdReleaseUnitTest"
